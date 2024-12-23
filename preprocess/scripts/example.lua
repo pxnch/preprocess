b599908e7f970fda7f96ac8b5cb7d7deea5cc4a9
Simon <60438167+pxnch@users.noreply.github.com>
2024-12-23 13:25:44 +0100

    More Example

--- preprocess/scripts/example.lua
+++ preprocess/scripts/example.lua
@@ -1,4 +1,5 @@
 local header = preprocess.create_header("example")
 header:add_macro("EXAMPLE_MACRO", "1")
 header:add_macro("RANDOM_NUMBER", math.random(1, 100))
+header:add_macro("TIME", os.date("%H:%M:%S"))
 header:write()
